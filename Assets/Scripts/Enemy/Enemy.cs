e3a6bf83cd45f5ea7e9662315fe6f4f75dcb2aa4
Marat <139675556+Muratich@users.noreply.github.com>
2025-07-20 14:46:05 +0300

    fix: ALL LEVELS ADDED

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,16 +1,34 @@
+using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 public class Enemy : NetworkBehaviour {
     public Health health;
+    public Animator animator;
 
     void Start() {
         if (health == null) { Debug.Log("Health not set to enemy!"); return; }
         if (NetworkObject == null) { Debug.Log("Network object not set to enemy!"); return; }
-        health.onDie.AddListener(EnemyDestroyLocal);
+        health.onDie.AddListener(ActivateAnim);
     }
 
-    public void EnemyDestroyLocal() {
+    private void ActivateAnim() {
+        StartCoroutine(DeathAnim());
+    }
+
+    IEnumerator DeathAnim() {
+        animator.Play("death");
+        yield return null;
+        AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(0);
+        while (!info.IsName("death")) {
+            yield return null;
+            info = animator.GetCurrentAnimatorStateInfo(0);
+        }
+        while (info.normalizedTime < 1f) {
+            yield return null;
+            info = animator.GetCurrentAnimatorStateInfo(0);
+        }
+
         if (IsServer)
             NetworkObject.Despawn();
         else
